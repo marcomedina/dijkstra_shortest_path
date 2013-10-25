eb359c581a9dfb38a3f496c8fa2a0ace29d1731c
Marco Medina <marco.medina@igobono.com>
2013-10-25 16:31:45 -0500

    Example added

--- dijkstras.rb
+++ dijkstras.rb
@@ -8,6 +8,20 @@ class Graph
   end
 
   def shortest_path from, to
+    
+  end
 
+  def to_s
+    @vertices.inspect
   end
 end
+
+#Example
+graph = Graph.new
+graph.add_vertex(1, {2 => 7, 3 => 9, 6 => 14})
+graph.add_vertex(2, {3 => 10, 4 => 15})
+graph.add_vertex(3, {1 => 9, 2 => 10, 4 => 11, 6 => 2})
+graph.add_vertex(4, {2 => 15, 5 => 6})
+graph.add_vertex(5, {6 => 9, 4 => 6})
+graph.add_vertex(6, {1 => 14, 3 => 2, 5 => 9})
+p graph.to_s
