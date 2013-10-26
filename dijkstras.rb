0e9fd4f7d3c566cd04fc816e7bf90f740ba62f9d
Marco Medina <marco.medina@igobono.com>
2013-10-25 20:28:09 -0500

    algorithm done

--- dijkstras.rb
+++ dijkstras.rb
@@ -1,27 +1,78 @@
 class Graph
   def initialize
     @vertices = {}
+    @INFINITY = 1 << 64
+    @path = []
   end
 
   def add_vertex name, edges
     @vertices[name] = edges
   end
 
-  def shortest_path from, to
-    
+  def dijkstra from
+    @distances = {}
+    @previous = {}
+    unvisited = @vertices.keys
+
+    @vertices.each do |vertex, value|
+      @distances[vertex] = @INFINITY
+      @previous[vertex] = nil
+    end
+    @distances[from] = 0
+
+
+    while unvisited.size > 0
+      current_node = nil
+      unvisited.each do |min|
+        if (!current_node) or (@distances[min] && @distances[min] < @distances[current_node])
+          current_node = min
+        end
+      end
+
+      if @distances[current_node] == @INFINITY
+        break
+      end
+
+      unvisited.delete current_node
+
+      @vertices[current_node].each do |nei, value|
+        alt = @distances[current_node] + @vertices[current_node][nei]
+        if alt < @distances[nei]
+          @distances[nei] = alt
+          @previous[nei] = current_node
+        end
+      end
+    end
   end
 
   def to_s
     @vertices.inspect
   end
+
+  def shortest_path from, to
+    dijkstra from
+    get_path to
+    @path
+  end
+
+  private
+  def get_path to
+    if !@previous[to].nil?
+      get_path @previous[to]
+    end
+    @path << to
+  end
 end
 
 #Example
 graph = Graph.new
-graph.add_vertex(1, {2 => 7, 3 => 9, 6 => 14})
-graph.add_vertex(2, {3 => 10, 4 => 15})
-graph.add_vertex(3, {1 => 9, 2 => 10, 4 => 11, 6 => 2})
-graph.add_vertex(4, {2 => 15, 5 => 6})
-graph.add_vertex(5, {6 => 9, 4 => 6})
-graph.add_vertex(6, {1 => 14, 3 => 2, 5 => 9})
-p graph.to_s
+graph.add_vertex('A', {'B' => 7, 'C' => 8})
+graph.add_vertex('B', {'A' => 7, 'F' => 2})
+graph.add_vertex('C', {'A' => 8, 'F' => 6, 'G' => 4})
+graph.add_vertex('D', {'F' => 8})
+graph.add_vertex('E', {'H' => 1})
+graph.add_vertex('F', {'B' => 2, 'C' => 6, 'D' => 8, 'G' => 9, 'H' => 3})
+graph.add_vertex('G', {'C' => 4, 'F' => 9})
+graph.add_vertex('H', {'E' => 1, 'F' => 3})
+
+puts graph.shortest_path 'A', 'H'
--- dijkstras_tests.rb
+++ dijkstras_tests.rb
@@ -10,4 +10,17 @@ class Graph_Test < Test::Unit::TestCase
     @graph.add_vertex('A', {'B' => 1, 'C' => 2})
     assert_equal({"A"=>{"B"=>1, "C"=>2}}, @graph.instance_variable_get(:@vertices), "Failed to add vertex")
   end
+
+  def test_shortest_path
+    @graph.add_vertex('A', {'B' => 7, 'C' => 8})
+    @graph.add_vertex('B', {'A' => 7, 'F' => 2})
+    @graph.add_vertex('C', {'A' => 8, 'F' => 6, 'G' => 4})
+    @graph.add_vertex('D', {'F' => 8})
+    @graph.add_vertex('E', {'H' => 1})
+    @graph.add_vertex('F', {'B' => 2, 'C' => 6, 'D' => 8, 'G' => 9, 'H' => 3})
+    @graph.add_vertex('G', {'C' => 4, 'F' => 9})
+    @graph.add_vertex('H', {'E' => 1, 'F' => 3})
+
+    assert_equal(['A', 'B', 'F', 'H'], @graph.shortest_path('A', 'H'), 'Failed to find shortest path')
+  end
 end
